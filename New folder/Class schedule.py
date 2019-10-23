8a980d46be1e8c27a355fc546c168e6fa537f075
Kephryn Merkley <kephryn.merkley@tooeleschools.org>
2019-10-23 13:19:32 -0600

    test

--- New folder/Class schedule.py
+++ New folder/Class schedule.py
@@ -2,14 +2,12 @@
 # 9/5
 # Class Schedule
 
-
 #  Blue day classes and times
 print("8 am - Marching band")
 print("9:15 - Health")
 print("10:30 - Math")
 print("Lunch/Bus to CLC")
 print("Python Coding 12:15")
-
 
 #  Silver day classes and times
 print("8 am - Seminary")
